94820bfa70286eddacef12338907ad6dceba500f
Escanor1986 <lionel.zovi@gmail.com>
2025-02-25 09:16:44 +0100

    Init backend - Spring Boot en place, MySQL branché

---
+++ chatop-backend/src/test/java/com/chatop/chatop_backend/ChatopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatop.chatop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
